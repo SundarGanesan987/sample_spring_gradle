433d9925413bbe078d67fbc191bf106f3f263481
Sundar <sundar.g.987@gmail.com>
2023-11-05 16:36:56 +0100

    new file:   build.gradle 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties

---
+++ src/main/java/com/example/sample/sample_spring_gradle/SampleSpringGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample.sample_spring_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringGradleApplication.class, args);
+	}
+
+}
